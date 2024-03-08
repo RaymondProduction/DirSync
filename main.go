c0799250f29cdf1e271ed3e6fa5a6775eac05478
Raymond Perminov <raymondproduction@ukr.net>
2024-03-09 00:25:17 +0200

    Systray

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+
+	"github.com/getlantern/systray"
+)
+
+func main() {
+	systray.Run(onReady, onExit)
+}
+
+func onReady() {
+	systray.SetIcon(getIcon("icon.png"))
+	systray.SetTitle("System tray")
+	systray.SetTooltip("Exaple for system tray")
+	mQuit := systray.AddMenuItem("Quit", "Quit from program")
+
+	go func() {
+		<-mQuit.ClickedCh
+		systray.Quit()
+		log.Println("Quit")
+	}()
+}
+
+func onExit() {
+	// clear if needed
+}
+
+func getIcon(filePath string) []byte {
+	icon, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatalf("Error during downloading icon: %v", err)
+	}
+	return icon
+}
